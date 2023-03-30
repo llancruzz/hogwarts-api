9e23f574fe86eb83cfcfec3086738888e8bd8b58
Alan Cruz da Silva <alancruzsilva@gmail.com>
2023-03-30 14:29:56 +0000

    feat: Create ProfileDetail view

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,4 @@
+from django.http import Http404
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Profile
@@ -14,4 +15,22 @@ class ProfileList(APIView):
         profiles = Profile.objects.all()
         # Add Serializer Profile
         serializer = ProfileSerializer(profiles, many=True)
+        return Response(serializer.data)
+
+
+class ProfileDetail(APIView):
+    """
+    Profile Detail View.
+    """
+
+    def get_object(self, pk):
+        try:
+            profile = Profile.objects.get(pk=pk)
+            return profile
+        except Profile.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        profile = self.get_object(pk)
+        serializer = ProfileSerializer(profile)
         return Response(serializer.data)
