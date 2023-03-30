b523cfb8884b9b6d1604b7bb41c9b3a63c71ec6f
Alan Cruz da Silva <alancruzsilva@gmail.com>
2023-03-30 12:56:32 +0000

    feat: Add signal to Profile model

--- profiles/models.py
+++ profiles/models.py
@@ -1,9 +1,9 @@
 from django.db import models
+from django.db.models.signals import post_save
 from django.contrib.auth.models import User
 
+
 # Create your models here.
-
-
 class Profile(models.Model):
     """
     Profile model.
@@ -23,3 +23,12 @@ class Profile(models.Model):
 
     def __str__(self):
         return f"{self.owner}'s profile"
+
+
+# Create a user instance every time a profile is created
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(owner=instance)
+
+
+post_save.connect(create_profile, sender=User)
