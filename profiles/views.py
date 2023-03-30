affe4a61254017c471f0b5979a8607e98874ca60
Alan Cruz da Silva <alancruzsilva@gmail.com>
2023-03-30 14:07:11 +0000

    feat: Create serializer file and add profile serializer and list view

--- profiles/views.py
+++ profiles/views.py
@@ -1,6 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Profile
+from .serializers import ProfileSerializer
 
 
 # Create your views here.
@@ -11,4 +12,6 @@ class ProfileList(APIView):
 
     def get(self, request):
         profiles = Profile.objects.all()
-        return Response(profiles)
+        # Add Serializer Profile
+        serializer = ProfileSerializer(profiles, many=True)
+        return Response(serializer.data)
