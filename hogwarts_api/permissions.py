ada783c2453a02bd73cfd361bc8022537b0a5ed6
Alan Cruz da Silva <alancruzsilva@gmail.com>
2023-03-30 15:02:24 +0000

    feat: Create permission.py file to allow only owner of the profile edit their profile details

---
+++ hogwarts_api/permissions.py
@@ -0,0 +1,13 @@
+from rest_framework import permissions
+
+
+class isOwnerOrReadOnly(permissions.BasePermission):
+    """
+    Create permission to check if the user is the owner.
+    Only Owner is allow to edit their profile details.
+    """
+
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return obj.owner == request.user
