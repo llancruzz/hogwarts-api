0699ba702cb973e1e3eeb6a4a4e98fc25b8b350d
Alan Cruz da Silva <alancruzsilva@gmail.com>
2023-04-01 12:51:40 +0000

    feat: Create LikeList generics views

--- likes/views.py
+++ likes/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from hogwarts_api.permissions import IsOwnerOrReadOnly
+from .models import Like
+from .serializers import LikeSerializer
 
-# Create your views here.
+
+class likeList(generics.ListCreateAPIView):
+    """
+    List all likes. Create a like if authenticated. 
+    The perform_create method associates the like with the logged in user.
+    """
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    serializer_class = LikeSerializer
+    queryset = Like.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(owner.self.request.user)
