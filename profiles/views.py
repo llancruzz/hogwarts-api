9db6aae857b728b5df381e1585080c5981d01fcc
Alan Cruz da Silva <alancruzsilva@gmail.com>
2023-04-02 17:38:00 +0000

    feat: Add profile filters using django filter library

--- profiles/views.py
+++ profiles/views.py
@@ -1,5 +1,6 @@
 from django.db.models import Count
 from rest_framework import generics, filters
+from django_filters.rest_framework import DjangoFilterBackend
 from .models import Profile
 from .serializers import ProfileSerializer
 from hogwarts_api.permissions import IsOwnerOrReadOnly
@@ -18,7 +19,10 @@ class ProfileList(generics.ListAPIView):
         followers_count=Count('owner__followed', distinct=True),
         following_count=Count('owner__following', distinct=True)
     ).order_by('-created_at')
-    filter_backends = [filters.OrderingFilter]
+    filter_backends = [filters.OrderingFilter, DjangoFilterBackend,]
+    filterset_fields = [
+        'owner__following__followed__profile',  # users profile
+    ]
     ordering_fields = [
         'posts_count', 'followers_count', 'following_count',
         'owner__following__created_at', 'owner__followed_created_at',
