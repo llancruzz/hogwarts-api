c4bf1d5b140ca64d0f24f44ae041ce4b3652ff8f
Alan Cruz da Silva <alancruzsilva@gmail.com>
2023-04-02 19:28:04 +0000

    feat: Create contact serializer for contact model

---
+++ contacts/serializers.py
@@ -0,0 +1,27 @@
+from rest_framework import serializers
+from django.contrib.humanize.templatetags.humanize import naturaltime
+from .models import Contact
+
+
+class ContactSerializer(serializers.ModelSerializer):
+    """
+    Serializer for the Contact Model.
+    """
+    owner = serializers.ReadOnlyField(source='owner.username')
+    profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
+    profile_id = serializers.ReadOnlyField(source='owner.profile.id')
+    created_at = serializers.SerializerMethodField()
+    updated_at = serializers.SerializerMethodField()
+
+    def get_created_at(self, obj):
+        return naturaltime(obj.created_at)
+
+    def get_updated_at():
+        return naturaltime(obj.updated_at)
+
+    class Meta:
+        model = Contact
+        fields = [
+            'id', 'owner', 'reason_contact', 'content',
+            'profile_id', 'profile_image', 'created_at', 'updated_at',
+        ]
