d46a41abe6030df3b459b30213c049de8f56c891
Alan Cruz da Silva <alancruzsilva@gmail.com>
2023-04-04 08:22:06 +0000

    feat: Create test for Contact, get and create contact

--- contacts/tests.py
+++ contacts/tests.py
@@ -1,3 +1,35 @@
-from django.test import TestCase
+from django.urls import reverse
+from django.contrib.auth.models import User
+from rest_framework.test import APITestCase
+from rest_framework import status
+from .models import Contact
 
-# Create your tests here.
+
+class ContactAPITest(APITestCase):
+    """
+    Contac tests:
+    Get Contact and create contact
+    """
+
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username='alan', password='pass')
+        self.contact = Contact.objects.create(
+            owner=self.user, reason_contact='Test Reason',
+            content='Test Content')
+        self.client.force_authenticate(user=self.user)
+
+    def test_get_contacts(self):
+        url = reverse('contacts')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_create_contact(self):
+        url = reverse('contacts')
+        data = {
+            'owner': self.user.id,
+            'reason_contact': 'Test Reason 2',
+            'content': 'Test Content 2'
+        }
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
--- contacts/urls.py
+++ contacts/urls.py
@@ -3,6 +3,6 @@ from contacts import views
 
 
 urlpatterns = [
-    path('contacts/', views.ContactList.as_view()),
+    path('contacts/', views.ContactList.as_view(), name='contacts'),
     path('contacts/<int:pk>/', views.ContactDetail.as_view()),
 ]
