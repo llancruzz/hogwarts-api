cab3c307d65e2edb2b5ee826d8d7a8b068abeeee
Alan Cruz da Silva <alancruzsilva@gmail.com>
2023-04-05 11:07:10 +0000

    chore: Install djangorestframework-simplejwt to connect token

--- hogwarts_api/settings.py
+++ hogwarts_api/settings.py
@@ -31,6 +31,20 @@ DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [(
+        'rest_framework.authentication.SessionAuthentication'
+        if 'DEV' in os.environ
+        else 'dj_rest_auth.jwt_auth.JWTCookieAuthentication'
+    )]
+}
+
+REST_USE_JWT = True
+JWT_AUTH_SECURE = True
+# Access token
+JWT_AUTH_COOKIE = 'my-app-auth'
+# Refresh token
+JWT_AUTH_REFRESH_COOKIE = 'my-refresh-token'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
