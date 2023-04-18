462d53deb330080b9407b8386c99e12413943b04
Alan Cruz da Silva <alancruzsilva@gmail.com>
2023-04-18 09:34:32 +0000

    feat: Create HouseProfile list and detail views

---
+++ houses/urls.py
@@ -0,0 +1,8 @@
+# Import necessary modules
+from django.urls import path
+from houses import views
+
+urlpatterns = [
+    path("houses/", views.HouseProfileList.as_view()),
+    path("houses/<int:pk>/", views.HouseProfileDetail.as_view),
+]
--- houses/views.py
+++ houses/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+# Import necessary modules
+from rest_framework import generics, permissions
+from hogwarts_api.permissions import IsOwnerOrReadOnly
+from .models import HouseProfile
+from .serializers import HouseProfileSerializer
 
-# Create your views here.
+
+class HouseProfileList(generics.ListCreateAPIView):
+    """
+    HouseProfileList generic view:
+    List all profile of hogwarts'house if logged in.
+    """
+
+    serializer_class = HouseProfileSerializer
+    permission_classes[permissions.IsAuthenticatedOrReadOnly]
+    queryset = HouseProfile.objects.all()
+
+
+class HouseProfileDetail(generics.RetrieveUpdateDestroyAPIView):
+    """
+    HouseProfileDetail generic view:
+    Retrieve a contact, or update or delete it by id if you own it.
+    """
+    serializer_class = HouseProfileSerializer
+    permission_classes = [permissions.IsAdminUser]
+    queryset = HouseProfile.objects.all()
