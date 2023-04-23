b09ad1d2aea38821f4521ad945667e8cc9579da7
Alan Cruz da Silva <alancruzsilva@gmail.com>
2023-04-23 19:16:08 +0000

    feat: Test deployment

--- houses/models.py
+++ houses/models.py
@@ -1,6 +1,6 @@
 # Import necessary modules
 from likes.models import Like
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
 from django.db import models
 from django.contrib.auth.models import User
@@ -19,8 +19,8 @@ class HouseProfile(models.Model):
         return self.house_name
 
 
-@receiver(post_save, sender=Like)
-def update_house_points(instance, created, **kwargs):
+@receiver(post_save, post_delete, sender=Like)
+def update_house_points(instance, created, deleted, **kwargs):
     """Create or update the house points"""
     # Check if an object was created
     if created:
@@ -30,4 +30,12 @@ def update_house_points(instance, created, **kwargs):
         house_to_update = HouseProfile.objects.get(house_name=house)
         # Set points as current total + 1
         house_to_update.current_points = house_to_update.current_points + 1
+        house_to_update.save()
+    if deleted:
+        # Get the house name from the post in the like
+        house = instance.post.house
+        # Get the house whos points need updated
+        house_to_update = HouseProfile.objects.get(house_name=house)
+        # Set points as current total + 1
+        house_to_update.current_points = house_to_update.current_points - 1
         house_to_update.save()
